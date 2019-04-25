817735325eb29805d8e56a411fdc5cf0667a244c
tmmons <t.m.mons.97@gmail.com>
2019-04-25 11:52:01 +0200

    the initial commit missed the function that actually loads datafiles, it is now added in. I also added necessary imports

--- load_ANA_file_functions.py
+++ load_ANA_file_functions.py
@@ -1,4 +1,22 @@
 #  usefull conversions
+import ROOT                        # To open root files
+import numpy as np                 # Fast
+import matplotlib.pyplot as plt    # Plotting
+import matplotlib.cm as cm         # colorbars
+import matplotlib                  # Plotting
+import datetime                    # For dates on axis
+from os import listdir             # To see what is in a folder
+from scipy.optimize import curve_fit # Fitting
+from scipy import interpolate
+from scipy.optimize import optimize # Fitting
+from scipy import signal
+import emcee
+import corner
+import scipy.optimize as op
+from matplotlib.ticker import MaxNLocator
+import io
+from iminuit import Minuit, describe, Struct
+
 y2s      = 365.25 * 24 * 3600 # years to seconds
 s2y      = 1. / y2s         # seconds to years
 toffset  = 70 * y2s       # the timestamp from labview is 70 years off
@@ -149,6 +167,141 @@ def files_to_open(set_date, i_want='ANA'):
 
 fnames = files_to_open(fromdate)
 calnames = files_to_open(fromdate,i_want='CAL')
-if len(calnames)>2:
-    print("Initializer::\tI've opened the files:\n",fnames[0].split('/')[-1],', ... ,',fnames[-1].split('/')[-1], '\t(total:',len(fnames),') and\n',
-      calnames[0].split('/')[-1],', ... ,',calnames[-1].split('/')[-1], '\t(total:',len(calnames),')')
+#if len(calnames)>2:
+#    print("Initializer::\tI've opened the files:\n",fnames[0].split('/')[-1],', ... ,',fnames[-1].split('/')[-1], '\t(total:',len(fnames),') and\n',
+#      calnames[0].split('/')[-1],', ... ,',calnames[-1].split('/')[-1], '\t(total:',len(calnames),')')
+
+
+in_ana = ['t0', 'time', 'channel', 'peak', 'rate', 'drate', 'e', 'res', 'temp', 'pres', 'bx', 'by',
+          'bz', 'btot', 'humid','frac','hv0', 'hv1','hv2','hv3','hv4', 'hv5','hv6','hv7','chi2ndf',
+          'bgrate', 'bgdrate', 'tot_error']
+in_cal = ['id', 'cal_tmin', 'cal_tmax', 'c0', 'c1', 'c2', 'chi2' ]
+
+time_start = []
+time_end   = []
+
+def file_opener(fnames = files_to_open('201509'), calnames =  files_to_open('201509','CAL')):
+    badfiles = []
+    for k in in_ana: # For all properties from in_ana make a list of this property
+        globals()[k] = []
+
+    # For each file in fnames open it and append the property to the right list
+    for i in range(len(fnames)):
+        f = ROOT.TFile.Open(fnames[i]) # Open the ANA file
+        try: # Sometimes the anafiles are corrupted, there will be no ana;1.
+            tree = f.Get("ana;1")
+        except ReferenceError: # If this happens, a reference error occurs, add this name to a list of bad files
+            badfiles.append(fnames[i])
+            continue
+        # A file can also be corrupted such that it is a TObject instead of a tree with leaves
+        if 'TObject' in str(type(tree)):
+            badfiles.append(fnames[i])
+            continue
+        # For each event in the tree, get its properties and append to the right list
+        for j , event in enumerate( tree) :
+            for k in in_ana:
+                if k == 'time': # Time is saved wrt t0, therefore need to be added to get the absolute time
+                    if j == 0: dt = (getattr(event, k))
+                    timestamp = getattr(event, k) + getattr(event, 't0')
+                    globals()[k].append(timestamp)
+                    time_start.append(timestamp - dt)
+                    time_end.append(  timestamp + dt)
+                elif k == 'frac': # for frac some events do not have this property, add a value of None here.
+                    try:
+                        globals()[k].append( getattr(event, k) )
+                    except AttributeError:
+                        globals()[k].append( None )
+                elif k == 'bgrate' or k == 'bgdrate': # for chi2ndf some events do not have this property, add a value of None here.
+                    try:
+                        globals()[k].append( getattr(event, k) )
+                    except AttributeError:
+                        globals()[k].append( -1 )
+                elif k == 'chi2ndf': # for chi2ndf some events do not have this property, add a value of None here.
+                    try:
+                        globals()[k].append( getattr(event, k) )
+                    except AttributeError:
+                        globals()[k].append( -1 )
+                elif k == 'tot_error': # for tot_error some events do not have this property, add a value of None here.
+                    try:
+                        globals()[k].append( getattr(event, k) )
+                    except AttributeError:
+                        globals()[k].append( -1 )
+                elif 'hv' in k: # for hv0-hv7 some events do not have this property, add a value of None here.
+                        try:
+                            globals()[k].append( getattr(event, k) )
+                        except AttributeError:
+                            globals()[k].append( None )
+                elif 'drate' in k:
+                    if getattr(event, k) == 'nan' or getattr(event, k) == 'NaN':
+                        print('Initializer::file_opener::A "None" occured')
+                        globals()[k].append( -10 )
+                    else:
+                        globals()[k].append( getattr(event, k) )
+                else: # Most poperties get added here to the right list
+                    globals()[k].append( getattr(event, k) )
+
+
+    for k in in_cal: # For all properties from in_cal make a list of this property
+        globals()[k] = []
+
+    # For each file in calnames open it and append the property to the right list
+    for i in range(len(calnames)):
+        f = ROOT.TFile.Open(calnames[i]) # open the CAL file
+        try: # Sometimes the calibration files are corrupted, there will be no cal;1.
+            tree = f.Get("cal;1")
+        except ReferenceError: #If this happens, a reference error occurs, add this name to a list of bad files
+                badfiles.append(calnames[i])
+                continue
+        # For each event in the tree, get its properties and append to the right list
+        for j , event in enumerate(tree) :
+            for k in in_cal:
+                # Some of the properties are not a simple leaf in the (cal) root file but are root vectors
+                # these are especially tricky to open. These can be opened by treating the rootvector as a
+                # variable and than converting it to a list before adding it to the list of this property
+                if k == 'c0' or k == 'c1' or k == 'c2' or k =='chi2':
+                    vec1 = getattr(event, k)
+                    list1 = [value for value in vec1] # This
+                    globals()[k].append(list1)
+                else:
+                    # Sometimes strange things happend with the calibration time therefore I check if the
+                    # timestamp does not have strange values.
+                    if k == 'cal_tmin':
+                        if getattr(event, k) < 1:
+                            print('cal time is negative')
+                            badfiles.append(calnames[i])
+                        elif getattr(event, k) > 4e9:
+                            print('cal time to big in file %s' %calnames[i])
+                            if calnames[i] not in badfiles:
+                                badfiles.append(calnames[i])
+                        else:
+                            globals()[k].append( getattr(event, k) )
+    # As written below, we now have some properties from the calibration that are in a vector. To convert these
+    # to a list we first create some lists:
+    for string in ['c0','c1', 'c2', 'chi2']:
+        for j in range(8):
+            name = string + '_chan_' + str(j)
+            globals()[name] = []
+            for i in range(len(c0)):
+                ccc = globals()[string]
+                globals()[name].append(ccc[i][j])
+
+    # And then convert these to np arrays.
+    for cx in ['c0','c1', 'c2', 'chi2']:
+        for ch in range(8):
+            name = cx + '_chan_' + str(ch)
+            in_cal.append(name)
+
+    for k in in_cal:
+        if k == 'id':
+            id_array = np.array(globals()[k])
+        else:
+            globals()[k] = np.array(globals()[k])
+    in_ana.append('time_start')
+    in_ana.append('time_end')
+    for k in in_ana:
+        globals()[k] = np.array(globals()[k])
+
+    if not badfiles == []: print ('Initializer::The files that have a problem are:\t', badfiles)
+    print ('Initializer::\tLoading done\n\tWe can access the properties:\n\t', *in_ana + in_cal)
+    print ('Initializer::\tEach of these properties is an np.array and can be used accourdingly')
+#file_opener(fnames, calnames)
